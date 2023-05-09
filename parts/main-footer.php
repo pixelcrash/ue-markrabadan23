fadac48b2394475510ebabf812bf5de824a9fe26
pixelcrash <kettele.georg@gmail.com>
2023-05-09 20:44:41 +0200

    test

--- parts/main-footer.php
+++ parts/main-footer.php
@@ -1,3 +1,18 @@
+<?php 
+  $args = array(
+  'numberposts' => -1,
+  'post_type' => 'post',
+  'orderby'    => 'menu_order',
+  'sort_order' => 'asc',
+  'fields' => 'ids'
+  );
+  $ps = get_posts($args);
+  if($ps):
+?>
 <ul>
-  <li><a href="">(1) project one</a></li>
+  <?php foreach($ps as $p: ?>
+  <li><a href="">(1) project one <?php var_dump($p); ?></a></li>
+  <?php endforeach; ?>
 </ul>
+
+<?php endif; ?>
--- parts/swiper-nav.php
+++
@@ -1,9 +0,0 @@
-<div class="slider-nav x-padding">
-  <div class="slider-nav-left ">
-    <a href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
-    <input type="hidden" value="<?php the_title(); ?>" id="emptyCaption">
-  </div>
-  <div class="slider-nav-right">
-    <a href="<?php echo get_home_url(); ?>/#information">Info</a>
-  </div>
-</div>
