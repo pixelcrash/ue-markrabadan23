c702aed0a1f91ba4180004075896945e5b692f69
pixelcrash <kettele.georg@gmail.com>
2023-05-09 22:39:10 +0200

    test

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php get_header();  ?>
+front page
+<header>
+  <?php get_template_part('parts/main', 'navigation'); ?>
+</header>
+
+<main class="text">
+  <?php the_content(); ?>
+</main>
+
+<footer>
+  <?php get_template_part('parts/main', 'footer'); ?>
+</footer>
+
+<?php get_footer();  ?>
