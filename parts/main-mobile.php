472f780f9d7685c788a59e1065f47c8b3cb28750
pixelcrash <kettele.georg@gmail.com>
2023-05-10 12:31:26 +0200

    test

--- parts/main-mobile.php
+++ parts/main-mobile.php
@@ -1,4 +1,6 @@
-<?php 
+<!-- This shows only on mobile  -->
+<div class="mobile-content">
+  <?php 
   $args = array(
   'numberposts' => -1,
   'post_type' => 'post',
@@ -11,35 +13,36 @@
   $ctn = 1; 
 ?>
 
-<?php foreach($ps as $p): ?>
-<?php $i = get_post_thumbnail_id( $p ); ?>
-<?php $img = ue_get_image( $i ); ?>
-<?php $gallery = get_field('gallery', $p); ?>
-<article class="mobile-view" id="project-<?=$i; ?>">
+  <?php foreach($ps as $p): ?>
+  <?php $i = get_post_thumbnail_id( $p ); ?>
+  <?php $img = ue_get_image( $i ); ?>
+  <?php $gallery = get_field('gallery', $p); ?>
+  <article class="mobile-view" id="project-<?=$i; ?>">
 
-  <div class="mobile-slider">
-    <?php if($gallery): ?>
-    <?php foreach($gallery as $g): ?>
-    <?php $gg = ue_get_image( $g ); ?>
-    <li>
+    <div class="mobile-slider">
+      <?php if($gallery): ?>
+      <?php foreach($gallery as $g): ?>
+      <?php $gg = ue_get_image( $g ); ?>
+      <li>
 
-      <img src="<?=$gg['srcM'][0]; ?>" alt="<?=$gg['alt']; ?>">
-    </li>
-    <?php endforeach; ?>
-    <?php endif; ?>
-  </div>
+        <img src="<?=$gg['srcM'][0]; ?>" alt="<?=$gg['alt']; ?>">
+      </li>
+      <?php endforeach; ?>
+      <?php endif; ?>
+    </div>
 
-  <div class="mobile-content">
-    <div class="accordion-title"><?php echo get_the_title($i); ?> <a href="#" class="">[more information]</a></div>
-    <div class="accordion-content"><?php echo get_the_content($i); ?></div>
-  </div>
+    <div class="mobile-content">
+      <div class="accordion-title"><?php echo get_the_title($i); ?> <a href="#" class="">[more information]</a></div>
+      <div class="accordion-content"><?php echo get_the_content($i); ?></div>
+    </div>
 
-  <a href="<?php echo get_permalink( $p ); ?>" data-img="<?=$img['srcL'][0]; ?>" data-style="<?=$img['style']; ?>">
-    (<?=$ctn; ?>) <?php echo get_the_title($p); ?>
-  </a>
+    <a href="<?php echo get_permalink( $p ); ?>" data-img="<?=$img['srcL'][0]; ?>" data-style="<?=$img['style']; ?>">
+      (<?=$ctn; ?>) <?php echo get_the_title($p); ?>
+    </a>
 
-</article>
-<?php $ctn++; endforeach; ?>
+  </article>
+  <?php $ctn++; endforeach; ?>
 
 
-<?php endif; ?>
+  <?php endif; ?>
+</div>
